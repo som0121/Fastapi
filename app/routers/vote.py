a8a0ffdfd2eae6890f01dfad59ee277e98f009cd
som <somparashar007@gmail.com>
2025-11-14 15:42:53 +0530

    Fix SyntaxError in vote router

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -22,8 +22,7 @@ def vote(vote:schema.Vote,db:session = Depends(database.get_db),current_user:int
     found_vote =vote_query.first()
     if(vote.dir==1):
         if found_vote:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"user {current_user.
-                                id} has already voted on this post{vote.post_id}")
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"user {current_user.id} has already voted on this post{vote.post_id}")
         new_vote = models.Vote(post_id =vote.post_id,user_id =current_user.id)
         db.add(new_vote)
         db.commit()
